79abeb65595f6c3fe05eb4ee9fc3d808bbd9e01a
Rein Van Imschoot <reinvanimschoot@gmail.com>
2021-02-13 10:53:40 +0100

    Adds exercises for debugging

---
+++ debugging/1_countdown.rb
@@ -0,0 +1,31 @@
+=begin
+
+Our countdown to launch isn't behaving as expected. Why? Change the code so that our program successfully counts down from 10 to 1.
+
+def decrease(counter)
+  counter -= 1
+end
+
+counter = 10
+
+10.times do
+  puts counter
+  decrease(counter)
+end
+
+puts 'LAUNCH!'
+
+=end
+
+def decrease(counter)
+  counter - 1
+end
+
+counter = 10
+
+10.times do
+  puts counter
+  counter = decrease(counter)
+end
+
+puts "LAUNCH!"
---
+++ debugging/2_hey_you.rb
@@ -0,0 +1,20 @@
+=begin
+
+String#upcase! is a destructive method, so why does this code print HEY you instead of HEY YOU?
+Modify the code so that it produces the expected output.
+
+def shout_out_to(name)
+  name.chars.each { |c| c.upcase! }
+
+  puts 'HEY ' + name
+end
+
+shout_out_to('you') # expected: 'HEY YOU'
+
+=end
+
+def shout_out_to(name)
+  puts "HEY " + name.upcase
+end
+
+shout_out_to("you") # expected: 'HEY YOU'
